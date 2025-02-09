0880b06fbd7d4e9ca191c4273bee45f403e7e26e
Tdev <tyler26daniel@gmail.com>
2025-02-09 04:49:32 -0800

    4th commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Hero from "@/components/hero";
+import HomeMain from "@/components/HomeMain";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import HomeMain from "./index/page";
+
 
 export default async function Home() {
   return (
