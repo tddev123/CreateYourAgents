462a64cc5201168dbc959c2f28bd92da072c69ac
Tdev <tyler26daniel@gmail.com>
2025-02-10 17:18:14 -0500

    19

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,20 +1,9 @@
-// app/payment-success/page.tsx
-import { Metadata } from "next";
+// pages/payment-success.tsx
+import { useRouter } from 'next/router'
 
-type SearchParamsProps = {
-  params: Record<string, string>;
-  searchParams: { amount?: string };
-}
-
-export const metadata: Metadata = {
-  title: 'Payment Success',
-};
-
-const PaymentSuccess = ({
-  params,
-  searchParams,
-}: SearchParamsProps) => {
-  const { amount = "0" } = searchParams;
+export default function PaymentSuccess() {
+  const router = useRouter()
+  const { amount } = router.query
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
@@ -22,11 +11,9 @@ const PaymentSuccess = ({
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
         <h2 className="text-2xl">You successfully sent</h2>
         <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
-          ${amount}
+          ${amount || '0'}
         </div>
       </div>
     </main>
   );
 }
-
-export default PaymentSuccess;
