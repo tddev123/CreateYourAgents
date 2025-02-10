4d0918fe129c52b374045b5cb8691e6e859b3834
Tdev <tyler26daniel@gmail.com>
2025-02-10 17:13:32 -0500

    17

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,20 +1,16 @@
 // app/payment-success/page.tsx
 import { Metadata } from "next";
 
-type Props = {
-  searchParams: {
-    amount: string;
-  };
-};
-
 export const metadata: Metadata = {
   title: 'Payment Success',
 };
 
-export default async function PaymentSuccess({
+export default function PaymentSuccess({
   searchParams,
-}: Props) {
-  const { amount } = searchParams;
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const amount = searchParams.amount as string;
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
