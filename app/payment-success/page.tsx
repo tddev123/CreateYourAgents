3d515b612a1b60b20aa1c76fc7448bd61db09156
Tdev <tyler26daniel@gmail.com>
2025-02-10 17:47:29 -0500

    20

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,17 +1,16 @@
-// pages/payment-success.tsx
-import { useRouter } from 'next/router'
-
-export default function PaymentSuccess() {
-  const router = useRouter()
-  const { amount } = router.query
-
+export default function PaymentSuccess({
+  searchParams: { amount },
+}: {
+  searchParams: { amount: string };
+}) {
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
         <h2 className="text-2xl">You successfully sent</h2>
+
         <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
-          ${amount || '0'}
+          ${amount}
         </div>
       </div>
     </main>
