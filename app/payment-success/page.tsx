ad0ed177393b748a39beb62e47c9d7bf0c475180
Tdev <tyler26daniel@gmail.com>
2025-02-10 16:09:44 -0500

    4

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,5 +1,11 @@
-export default function PaymentSuccess({ searchParams }: { searchParams: { amount: string } }) {
-  const { amount } = searchParams;
+export default function PaymentSuccess({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
+  const amount = Array.isArray(searchParams.amount)
+    ? searchParams.amount[0]
+    : searchParams.amount;
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
@@ -8,7 +14,7 @@ export default function PaymentSuccess({ searchParams }: { searchParams: { amoun
         <h2 className="text-2xl">You successfully sent</h2>
 
         <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
-          ${amount}
+          ${amount ?? "0.00"}
         </div>
       </div>
     </main>
