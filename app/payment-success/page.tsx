4596fa3dcb068032c326b327def97d26fccc39eb
Tdev <tyler26daniel@gmail.com>
2025-02-10 16:05:52 -0500

    3

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,18 +1,16 @@
-export default function PaymentSuccess({
-    searchParams: { amount },
-  }: {
-    searchParams: { amount: string };
-  }) {
-    return (
-      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
-        <div className="mb-10">
-          <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-          <h2 className="text-2xl">You successfully sent</h2>
-  
-          <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
-            ${amount}
-          </div>
+export default function PaymentSuccess({ searchParams }: { searchParams: { amount: string } }) {
+  const { amount } = searchParams;
+
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
+        <h2 className="text-2xl">You successfully sent</h2>
+
+        <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
+          ${amount}
         </div>
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
+}
