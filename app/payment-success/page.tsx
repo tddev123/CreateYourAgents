a09c8c33c4360d8b9f06ed4ac411ff48f1c93a4f
Tdev <tyler26daniel@gmail.com>
2025-02-10 17:09:54 -0500

    15

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,11 +1,30 @@
-import PaymentSuccess from "@/components/PaymentSuccess";
+// app/payment-success/page.tsx
+import { Metadata } from "next";
 
-interface PageProps {
-  searchParams: { amount?: string };
-}
+type SearchParams = {
+  searchParams: {
+    amount: string;
+  };
+};
 
-export default function PaymentSuccessPage({ searchParams }: PageProps) {
-  const amount = searchParams.amount ?? "0"; // Ensure amount is always a string
+export const metadata: Metadata = {
+  title: 'Payment Success',
+};
 
-  return <PaymentSuccess searchParams={{ amount }} />;
+export default function PaymentSuccess({
+  searchParams,
+}: SearchParams) {
+  const { amount } = searchParams;
+
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-400 to-green-600">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
+        <h2 className="text-2xl">You successfully sent</h2>
+        <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
+          ${amount}
+        </div>
+      </div>
+    </main>
+  );
 }
