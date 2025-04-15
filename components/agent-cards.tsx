d6676a6b6ae0c26dfb2428a0d7ac7fa7892e8b20
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-04-15 00:05:49 -0400

    Update agent-cards.tsx

--- components/agent-cards.tsx
+++ components/agent-cards.tsx
@@ -11,11 +11,11 @@ interface Agent {
 }
 
 const agents: Agent[] = [
-  { id: 1, name: "Instagram", image: "/static/images/image_fx_ (17).jpg"},
+  { id: 1, name: "Instagram", image: "/static/images/image_fx_ (17).jpg", video: "/static/images/Youtubeshortsbot.mp4" },
   { id: 2, name: "Facebook", image: "/static/images/image_fx_ (19).jpg", video: "/static/images/fbagentvid.mp4" },
-  { id: 3, name: "Universal", image: "/static/images/alien11111.png"},
+  { id: 3, name: "Universal", image: "/static/images/alien11111.png", video: "/static/images/Youtubeshortsbot.mp4" },
   { id: 4, name: "Youtube", image: "/static/images/image_fx_ (24).jpg", video: "/static/images/Youtubeshortsbot.mp4" },
-  { id: 5, name: "CustomerService", image: "/static/images/image_fx_ (15).jpg"},
+  { id: 5, name: "CustomerService", image: "/static/images/image_fx_ (15).jpg", video: "/static/images/Youtubeshortsbot.mp4" },
 ];
 
 const AgentCards: React.FC = () => {
@@ -155,3 +155,4 @@ const AgentCards: React.FC = () => {
 };
 
 export default AgentCards;
+
